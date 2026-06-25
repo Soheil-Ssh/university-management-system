ec8ca120ec3d534bfee726841e857d8c9837e5af
Soheil Shahsavan <soheil.ssh1280@gmail.com>
2026-06-26 03:27:02 +0330

    feat: add validation behavior in shared ke

---
+++ src/SharedKernel/SharedKernel.Abstractions/Behaviors/ValidationBehavior.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace SharedKernel.Abstractions.Behaviors;
+
+public class ValidationBehavior<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validators)
+    : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+{
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        if (!validators.Any())
+            return await next(cancellationToken);
+
+        var context = new ValidationContext<TRequest>(request);
+
+        var failures = (await Task.WhenAll(
+                validators.Select(v => v.ValidateAsync(context, cancellationToken))))
+            .SelectMany(x => x.Errors)
+            .Where(x => x != null)
+            .ToList();
+
+        if (failures.Count == 0)
+            return await next(cancellationToken);
+
+        throw new ValidationException(failures);
+    }
+}
