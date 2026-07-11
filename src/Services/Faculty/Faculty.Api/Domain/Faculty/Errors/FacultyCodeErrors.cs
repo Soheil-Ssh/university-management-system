4bacc95ba69785eeb4abc4e9f272d742a9cc1214
Soheil Shahsavan <soheil.ssh1280@gmail.com>
2026-07-11 20:53:55 +0330

    fix

--- src/Services/Faculty/Faculty.Api/Domain/Faculty/Errors/FacultyCodeErrors.cs
+++ src/Services/Faculty/Faculty.Api/Domain/Faculty/Errors/FacultyCodeErrors.cs
@@ -1,10 +1,9 @@
-﻿using SharedKernel.Domain.Error;
-
-namespace Faculty.Api.Domain.Faculty.Errors;
+﻿namespace Faculty.Api.Domain.Faculty.Errors;
 
 public static class FacultyCodeErrors
 {
-    public static readonly Error Empty = new("UnitCode.Empty", "Unit code cannot be empty.", ErrorType.Validation);
-    public static readonly Error InvalidFormat = new("UnitCode.InvalidFormat", "Unit code format is invalid. Expected format is UMS_CO_0001.", ErrorType.Validation);
-    public static readonly Error NumberOutOfRange = new("UnitCode.NumberOutOfRange", "Unit code number must be between 1 and 9999.", ErrorType.Validation);
+    public static readonly Error Empty = new("FacultyCode.Empty", "Faculty code cannot be empty.", ErrorType.Validation);
+    public static readonly Error InvalidFormat = 
+        new("FacultyCode.InvalidFormat", "Faculty code format is invalid. Expected format is UMS_FAC_FACULTY_0001.", ErrorType.Validation);
+    public static readonly Error NumberOutOfRange = new("FacultyCode.NumberOutOfRange", "Faculty code number must be between 1 and 9999.", ErrorType.Validation);
 }
