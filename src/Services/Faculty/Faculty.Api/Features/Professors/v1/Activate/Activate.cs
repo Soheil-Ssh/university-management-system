65dddf47589becee0825454510b870ab1fb3cd88
Soheil Shahsavan <soheil.ssh1280@gmail.com>
2026-07-15 07:11:08 +0330

    feat: add activate profe

---
+++ src/Services/Faculty/Faculty.Api/Features/Professors/v1/Activate/Activate.cs
@@ -0,0 +1,41 @@
+﻿namespace Faculty.Api.Features.Professors.v1.Activate;
+
+public static class Activate
+{
+    public sealed record Command(Guid ProfessorId) : ICommand<Result>;
+
+    public sealed class Handler(IProfessorRepository professorRepository, IUnitOfWork unitOfWork) : ICommandHandler<Command, Result>
+    {
+        public async Task<Result> Handle(Command request, CancellationToken cancellationToken)
+        {
+            var professor = await professorRepository.GetByIdAsync(new ProfessorId(request.ProfessorId), cancellationToken);
+            if (professor is null)
+                return ProfessorErrors.NotFound;
+
+            var activateResult = professor.Activate();
+
+            if (activateResult.IsFailure)
+                return activateResult.Error;
+
+            await unitOfWork.SaveAsync(cancellationToken);
+            return Result.Success();
+        }
+    }
+
+    public sealed class Endpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPatch("api/v{v:apiVersion}/professors/{professorId:guid}/activate",
+                    async (Guid professorId, ISender sender, CancellationToken cancellationToken) =>
+                    {
+                        var result = await sender.Send(new Command(professorId), cancellationToken);
+                        return result.ToHttpResult();
+                    })
+                //.RequirePermission(PermissionCodes.Faculty.ProfessorsActivate)
+                .Version(app, 1.0)
+                .WithName("ActivateProfessor")
+                .WithTags("Professors");
+        }
+    }
+}
